07118e5bca8bde0012693e9c0fa266f72e22f58e
Robson Marques <robsonmarques@gmail.com>
2013-10-22 23:20:51 -0200

    Correct attr_format instance variable

--- lib/mascherari/attr_masked.rb
+++ lib/mascherari/attr_masked.rb
@@ -2,20 +2,21 @@ module Mascherari
   module AttrMasked
     def attr_masked(*attrs, options)
       attrs.each do |attr|
-        format = "#{attr}_format"
-        masked = "#{attr}_masked"
-        unmasked = "#{attr}_unmasked"
+        formatter = "#{attr}_format"
+        masked    = "#{attr}_masked"
+        unmasked  = "#{attr}_unmasked"
+        ivar      = "@#{formatter}"
 
-        define_method format do
-          @format ||= Formatter.new options
+        define_method formatter do
+          instance_variable_get(ivar) || instance_variable_set(ivar, Formatter.new(options))
         end
 
         define_method masked do
-          send(format).mask send(attr)
+          send(formatter).mask send(attr)
         end
 
         define_method unmasked do
-          send(format).unmask send(attr)
+          send(formatter).unmask send(attr)
         end
       end
     end
